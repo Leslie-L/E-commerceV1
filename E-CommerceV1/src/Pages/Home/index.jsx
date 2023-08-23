b30ebda3faa240a7d47b7c14d15f9f85b0f1e9dc
Leslie-L <leslie.centeno@galileo.edu>
2023-08-23 15:29:15 -0600

    Agregando cards

--- E-CommerceV1/src/Pages/Home/index.jsx
+++ E-CommerceV1/src/Pages/Home/index.jsx
@@ -1,10 +1,27 @@
 import Layout from "../../Components/Layout";
+import Card from "../../Components/Card";
+import { useEffect, useState } from "react";
 function Home() {
-  
+  const [items, setItems] = useState([]);
+  useEffect(()=>{
+    const getData =  async () =>{
+      const response =  await fetch('https://fakestoreapi.com/products');
+      const data = await response.json();
+      setItems(data);
+    }
+    getData();
+  },[])
 
   return (
     <Layout>
       Home
+      <section className="grid grid-cols-4 gap-4">
+        {
+          items.map((item)=>{
+            return <Card key={item.id} data={item}></Card>
+          })
+        }
+      </section>
     </Layout>
   )
 }
